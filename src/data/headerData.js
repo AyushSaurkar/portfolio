cd1e4711708d8fea09546152acded7b1f65611ee
Ayush Saurkar <ayushsaurkar@gmail.com>
2024-10-27 15:26:51 +0530

    portfolioadded

---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from "../assets/pdf/resume.pdf";
+
+export const headerData = {
+  name: "Ayush Saurkar",
+  title: "Hello! I'am",
+  desciption:
+    "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+  image: "https://media.licdn.com/dms/image/v2/D4D03AQFTap8iGffUMQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1722527872328?e=1735171200&v=beta&t=QXK9TDJGiD-mpOpaiyW-L06zpr7tOFtF6EW4r3UQgWs",
+  resumePdf: resume,
+};
